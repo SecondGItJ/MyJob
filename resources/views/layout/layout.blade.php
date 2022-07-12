f619021f6bf7ebfa55fa428954153f22435fc0ba
brick123475 <uss940815@gmail.com>
2022-07-12 15:34:35 +0900

    220712 :: 홈페이지 제작 1. 로고 생성 2. css 파일 작성 3. 메인 페이지 수정 4. 레이아웃 수정

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -5,61 +5,73 @@
     <meta charset="utf-8">
     <title>MyPage</title>
 
+    <!-- bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- fullpage css -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.9/fullpage.min.css">
 
+    <!-- style css -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- jquery js -->
     <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+
+    <!-- fullpage js -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.9/fullpage.min.js"></script>
+
+    <!-- bootstrap js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 
     @yield('script')
 
 </head>
 
 <body>
-    <header>
-        <div class="header_cont">
-            <p class="logo_1"><a href="/"><img src="images/logo_1.png" /></a></p>
-            <ul>
-                <li><a href="#">기업정보</a></li>
-                <li><a href="#">사업분야</a></li>
-                <li><a href="#">기술분야</a></li>
-                <li><a href="#">고객분야</a></li>
-            </ul>
-        </div>
-
-        <div class="line"></div>
-
-        <nav id="menu">
-            <div id="menu_t">
-                <ul>
-                    <li><a href="#">회사소개</a></li>
-                    <li><a href="#">구성원소개</a></li>
-                    <li><a href="#">연혁/인증</a></li>
-                    <li><a href="#">주요 고객사</a></li>
-                    <li><a href="#">포트폴리오</a></li>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/"><img src="images/logo.png" alt="loading..."></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
                 </ul>
-
-                <ul>
-                    <li><a href="#">Smart Factory</a></li>
-                    <li><a href="#">출입관리</a></li>
-                    <li><a href="#">출장관리</a></li>
-
-                </ul>
-
-                <ul>
-                    <li><a href="#">웹 사이트</a></li>
-                    <li><a href="#">모바일</a></li>
-                    <li><a href="#">응용 프로그램</a></li>
-                </ul>
-
-                <ul>
-                    <li><a href="#">공지사항</a></li>
-                    <li><a href="#">견적문의</a></li>
-                    <li><a href="#">오시는 길</a></li>
-                </ul>
-
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
             </div>
-        </nav>
-    </header>
+        </div>
+    </nav>
     @yield('content')
 </body>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,21 +23,4 @@
 @endsection
 
 @section('content')
-    <section id="fullpage">
-        <div class='section' style="background-color: red">
-            <h1>1</h1>
-        </div>
-
-        <div class='section' style="background-color: yellow">
-            <h1>2</h1>
-        </div>
-
-        <div class='section' style="background-color: blue">
-            <h1>3</h1>
-        </div>
-
-        <div class='section' style="background-color: green">
-            <h1>4</h1>
-        </div>
-    </section>
 @endsection
